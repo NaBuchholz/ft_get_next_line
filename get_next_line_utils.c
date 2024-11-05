c2bf3b41d3ba88475dcea71e9fe91a5dd6aabd2b
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2024-11-05 13:38:41 -0300

    Refactor: Improve get_next_line function
    - Add static helper functions get_line, update_stash, and read_and_stash
    - Update get_next_line to use the new helper functions
    - Improve code readability and maintainability

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,61 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 13:06:56 by nbuchhol          #+#    #+#             */
-/*   Updated: 2024/10/29 16:01:34 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2024/11/05 13:33:47 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (!s)
+		return (NULL);
+	while (*s)
+	{
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
+	}
+	if ((char)c == '\0')
+		return ((char *)s);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*str;
+	size_t	i;
+	size_t	j;
+
+	if (!s1)
+	{
+		s1 = malloc(sizeof(char));
+		s1[0] = '\0';
+	}
+	str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	free(s1);
+	return (str);
+}

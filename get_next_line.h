ef7b67b67868d3cbe47b0fff72a15b089b71e64a
Nathalia Vitoria Buchholz <nbuchhol@c1r5p3.42sp.org.br>
2024-11-05 13:11:08 -0300

    Refactor: get_next_line.h and remove main.c

--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,23 @@
 /*   By: nbuchhol <nbuchhol@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 13:06:59 by nbuchhol          #+#    #+#             */
-/*   Updated: 2024/10/29 16:05:41 by nbuchhol         ###   ########.fr       */
+/*   Updated: 2024/11/05 13:09:36 by nbuchhol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
-
 # define GET_NEXT_LINE_H
+
 # include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*get_next_line(int fd);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(const char *s);
+
 #endif
